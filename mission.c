6d25893013fb739229ee0b5ecdb10ec587b3302e
31012 <128569890+day0235@users.noreply.github.com>
2023-04-05 04:38:17 +0000

    Initial commit

--- mission.c
+++ mission.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-main(){
+main() {
   int a;
-  scanf("%d" , &a); // 정수 입력받기
-  if(a%2==0){
+  scanf("%d", &a); // 정수 입력받기
+  if (a % 2 == 0) {
     printf("짝수입니다.");
-  }
-  else{
+  } else {
     printf("홀수입니다.");
   }
 }
