ed6520f7a592f75e25bec8c379f1477dc98ee7b7
day0235 <128569890+day0235@users.noreply.github.com>
2023-05-16 06:24:56 +0000

    switch 끝

---
+++ .ccls-cache/@home@runner@322-test/mission_myscore.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+
+// main(){
+// int x;
+// printf("90점 이상:A, 80점 이상:B, 70점 이상:C, 60점 이상:D, 50점 이상:E, 50점 미만:F\n");
+// scanf("%d", &x);
+  
+// if(x>=90)
+//   printf("A");
+// else if(x>=80)
+//   printf("B");
+// else if(x>=70)
+//   printf("C");
+// else if(x>=60)
+//   printf("D");
+// else if(x>=50)
+//   printf("E");
+// else
+//   printf("F");
+// printf("학점");
+// }
